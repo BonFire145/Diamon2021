d8f936917c5f36252123c0ebf6dd9592706b7399
김택용 <alq145@naver.com>
2021-10-17 05:54:09 +0900

    모델 입출력

--- DeepLearning-Model/torch-lab-05-2-logistic_regression_diabetes.py
+++ DeepLearning-Model/torch-lab-05-2-logistic_regression_diabetes.py
@@ -1,11 +1,15 @@
-# Lab 5 Logistic Regression Classifier
+import os
 import torch
 from torch.autograd import Variable
 import numpy as np
 
+path = os.path.dirname(os.path.abspath(__file__))
+modelFile = os.path.join(path, "model/model_static.pt")
+csvFile = os.path.join(path, "data-03-diabetes.csv")
+
 torch.manual_seed(777)  # for reproducibility
 
-xy = np.loadtxt('data-03-diabetes.csv', delimiter=',', dtype=np.float32)
+xy = np.loadtxt(csvFile, delimiter=',', dtype=np.float32)
 x_data = xy[:, 0:-1]
 y_data = xy[:, [-1]]
 
@@ -19,6 +23,10 @@ Y = Variable(torch.from_numpy(y_data))
 linear = torch.nn.Linear(8, 1, bias=True)
 sigmoid = torch.nn.Sigmoid()
 model = torch.nn.Sequential(linear, sigmoid)
+
+# Load Model
+model.load_state_dict(torch.load(modelFile))
+model.eval()
 
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
@@ -37,4 +45,9 @@ for step in range(10001):
 # Accuracy computation
 predicted = (model(X).data > 0.5).float()
 accuracy = (predicted == Y.data).float().mean()
-print("\nHypothesis: ", hypothesis.data.numpy(), "\nCorrect (Y): ", predicted.numpy(), "\nAccuracy: ", accuracy)
+print("\nHypothesis: ", hypothesis.data.numpy(), "\nCorrect (Y): ",
+      predicted.numpy(), "\nAccuracy: ", accuracy)
+print("신뢰도: {:.2f}%".format(accuracy*100))
+
+# Save Model
+torch.save(model.state_dict(), modelFile)
